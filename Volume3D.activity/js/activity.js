612518bb2d1a290148121ee9f8628b08985366cc
Vishal K <116670999+Vishalk91-4@users.noreply.github.com>
2024-04-15 08:04:10 +0530

    Update activity.js

--- Volume3D.activity/js/activity.js
+++ Volume3D.activity/js/activity.js
@@ -1,22 +1,25 @@
-// Rebase require directory
 requirejs.config({
-	baseUrl: "lib",
-	paths: {
-		activity: "../js"
-	}
+    baseUrl: "lib",
+    paths: {
+        activity: "../js",
+        volume3D: "../js/board" 
+    }
 });
 
-// Vue main app
-var app = new Vue({
-	el: '#app',
-	data: {
-		displayText: ''
-	},
-	methods: {
-		initialized: function () {
-			// Sugarizer initialized
-			var environment = this.$refs.SugarActivity.getEnvironment();
-			this.displayText = "Hello " + environment.user.name + "!";	
-		}
-	}
+requirejs(['volume3D'], function(Volume3D) {
+    var app = new Vue({
+        el: '#app',
+        components: {
+            'volume-3d': Volume3D
+        },
+        data: {
+            displayText: ''
+        },
+        methods: {
+            initialized: function () {
+                var environment = this.$refs.SugarActivity.getEnvironment();
+                this.displayText = "Hello " + environment.user.name + "!";    
+            }
+        }
+    });
 });
